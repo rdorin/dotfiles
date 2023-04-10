767eb1df6ea55102e49249248a06a8761820a090
Randall Dorin <rdorin@Katherines-MacBook-Air.local>
2023-04-09 22:56:19 -0400

    adding starship

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -7,15 +7,14 @@ alias ...='cd ../..'
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+alias nv=nvim
+
 # Helpers
 alias grep='grep --color=auto'
 alias df='df -h' # disk free, in Gigabytes, not bytes
 alias du='du -h -c' # calculate disk usage for a folder
 
 alias lpath='echo $PATH | tr ":" "\n"' # list the PATH separated by new lines
-
-# Applications
-alias ios='open -a /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
 
 # Hide/show all desktop icons (useful when presenting)
 alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
